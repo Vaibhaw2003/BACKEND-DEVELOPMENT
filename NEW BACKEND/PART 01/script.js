90e491fa87c82c6e2b12a9844c25cb8ce6c12e64
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-02 23:51:54 +0530

    ok

--- NEW BACKEND/PART 01/script.js
+++ NEW BACKEND/PART 01/script.js
@@ -5,6 +5,9 @@
 
 // example -- 
 
-var a = 12;
-b= a;
-console.log(b)
+// var a = [12,33,44,55 ];
+//  var b=[...a]
+// b.pop('33')
+
+// falsy valuse -- 0, false, NaN, null, undefined," ", document.all .
+false || 12
