65b9853b95a02c5b60cb15cb0e5589280c068ce6
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-02 22:57:12 +0530

    new

--- NEW BACKEND/PART 01/script.js
+++ NEW BACKEND/PART 01/script.js
@@ -1,1 +1,10 @@
-alert("hey");
+// primitive and reference
+
+// () ,{} .[] -- all brackets are call refereced.
+// other are called primitive.
+
+// example -- 
+
+var a = 12;
+b= a;
+console.log(b)
