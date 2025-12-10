6c298c218216fa7bed19043adedb1a46cd64529d
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-11 01:01:47 +0530

    backend-day04

--- NEW BACKEND/BACKEND-V04/script.js
+++ NEW BACKEND/BACKEND-V04/script.js
@@ -1,10 +1,21 @@
-const http = require('http');
+// EXPRESS.JS FRAMWORK HAI.
 
-const server = http.createServer(function(res , req){
-    req.end("harsh singh ");
+// introduction to framework .
+// express js ek npm package hai.
 
+// framework - ak flow me chijo ko rakhana hi framework hai.
+
+// manage everything from reciving the request(req ) and giving the response(res).
+
+// ----------first code of express---------
+
+const express = require('express'); // yaha pe express ki sari power const express me store ho gayi hai.
+const app = express(); // or yaha pe const express ki sari power cost app me hai.( or express js ak function hai)
+
+// with  the help of above both line we created routes.
+
+app.get('/', function(req, res){
+    res.send("vaibhaw singh from the seerver")
 });
 
-
-server.listen(3000);
-
+app.listen(3000);
