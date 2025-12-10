8d99caf8a97cd764fe6b1b642432368c9998d5d8
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-11 00:10:05 +0530

    day-03 completed

--- NEW BACKEND/BACKEND-V03/script.js
+++ NEW BACKEND/BACKEND-V03/script.js
@@ -15,3 +15,10 @@ Server.listen(4000);
 // install - npm i (package name)
 //uninstall - npm uninstall package name.
 // partikular version ke liye - npm i package name@ 4.22.2
+
+//dependancies - package and package ki dependencies.
+// devdependencies  - aise package jo sirf development mein kam ati hai, baad me unka kam nhi hota.
+//scripts - kisi bhi name se script bana sakte ho example [ chacha , start test]. eshka use server ko run karane me hota hai.
+// packages - chalk [ use for colour full code line] / concurrency [ use for running two server at a time ] 
+
+console.log("cahcah")
