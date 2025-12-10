90f9256205ff57b04c253d7497bad4b220b39050
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-10 23:32:24 +0530

    backend-day03

--- NEW BACKEND/BACKEND-V03/script.js
+++ NEW BACKEND/BACKEND-V03/script.js
@@ -1,3 +1,5 @@
+
+/*
 const http = require("http");
 
 const Server = http.createServer(function(res, req){   // ya pe [ res , req] ko same way me likhna jaruri hai nhi to server run nhi hoga.
@@ -5,3 +7,11 @@ const Server = http.createServer(function(res, req){   // ya pe [ res , req] ko 
 });
 
 Server.listen(4000);
+
+*/
+
+// npm understanding / installing / node modules / dependencise / devdependancise / scripts [ all about thise video ]
+// npm - node package manager. 
+// install - npm i (package name)
+//uninstall - npm uninstall package name.
+// partikular version ke liye - npm i package name@ 4.22.2
