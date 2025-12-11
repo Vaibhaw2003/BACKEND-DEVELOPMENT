199d10d160c06cb08b2cf90670055bd3ad456693
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-12 01:25:44 +0530

    day-05 completed

--- NEW BACKEND/BACKEND-V05/scripts.js
+++ NEW BACKEND/BACKEND-V05/scripts.js
@@ -1,3 +1,28 @@
 //---------------[ form handling and session and cookise  ]-----------
 
+// form handling and working with the forms.
+
+//  install one package name is - [ cookie-parser ]
+
+// ------------session - cookie----------
+
+// hum loge kuchh bhi data frontent par browser par rakha skte hai and jab bji app kuchh bhi request backend par karoge wo frontent par save data atomatically backend par chala jayega.
+
+// cookie matlab jo data frontent pe save hai .( manally used in login )
+
+//session mtlab login se lekar logout karane tak ke time ko session kahte hai.
+
+// cookie have one( unik id and string ) that use to cummunicate frontent to baackend during login.
+
+// blob - unredable stream, not directally red.
+
+//blob ko red karne ke liye hame json ka use karte hai.
+
+// json blob ko redable formate me banata hai.
+// eaxample-- 
+
+// app.use(express.json());
+//app.use(express.unlencoded({ extended : true }));
+
+
 
