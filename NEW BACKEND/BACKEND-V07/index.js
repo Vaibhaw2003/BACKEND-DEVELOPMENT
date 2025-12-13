c92b2afb02d404706396a0bd2469b340953ef3bb
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-13 23:57:41 +0530

    day-07 completed

---
+++ NEW BACKEND/BACKEND-V07/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("node:path");
+const app = express();
+const fs = require("fs"); // file handling me use hota hai curde operation.
+
+app.use(express.json());
+app.set('view engine', 'ejs');// eshame app .set ka use hota hai views wala folder ko cahlane ke liye ./ spelling error nhi hona chahiye or ejs install ho. 
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname, "public")))// ye use hota hai path dene ke liye public file ke liye.
+
+app.get('/', function(req, res){
+    fs.readdir(`./files`, function(err, files){
+       //console.log(files);
+       res.render("index", {files : files}) // we use rendar because of we use views engine , ejs.
+       });
+    })
+    
+
+app.listen(3000);
+
